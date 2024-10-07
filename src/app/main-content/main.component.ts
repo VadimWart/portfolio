0257b99f567236622e913e82f4fdecff6519767e
VadimWart <wartvadim@gmail.com>
2024-10-07 22:22:54 +0200

    first_page_add

--- src/app/main-content/main.component.ts
+++ src/app/main-content/main.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { WhyMeComponent } from './why-me/why-me.component';
+import { FirstPageComponent } from './first-page/first-page.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    SkillsComponent,
+    ContactComponent,
+    MyWorkComponent,
+    WhyMeComponent,
+    FirstPageComponent,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
