b08554606fbbc94de68e695995cad80bf75fa303
VadimWart <wartvadim@gmail.com>
2024-10-16 15:51:32 +0200

    privacy_policy_add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main-content/main.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './main-content/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'imprint', component: ImprintComponent },
+  { path: 'privacy', component: PrivacyPolicyComponent },
 ];
