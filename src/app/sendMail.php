0b716c237d4f427b21a9fb01f04e7f9ae6fc4e24
VadimWart <wartvadim@gmail.com>
2024-10-16 16:02:01 +0200

    send_email_add

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'phillip@phillip-marcel-sauer.com';  
+            $recipient = 'wartvadim@gmail.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
