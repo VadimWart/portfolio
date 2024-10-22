c3f9290986bc2be526bc4206f6e43c5a71a28668
VadimWart <wartvadim@gmail.com>
2024-10-22 23:02:44 +0200

    update_message_alert

--- src/app/main-content/message/message.component.ts
+++ src/app/main-content/message/message.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-message',
+  selector: 'app-message-response',
   standalone: true,
+  inputs: ['isMessageOut'],
   imports: [CommonModule, TranslateModule],
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss',
 })
 export class MessageComponent {
   public translate = inject(TranslateService);
+  @Input() isMessageOut!: boolean; // Definiert den Input, den es erhält
 }
